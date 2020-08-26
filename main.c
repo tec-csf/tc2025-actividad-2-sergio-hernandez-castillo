85618678b814e5851cbb307975d99007be68eede
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-08-25 22:23:18 -0500

    Se añadió el struct de Libro.

--- main.c
+++ main.c
@@ -7,6 +7,11 @@ Descripción: Actividad 2 - Apuntadores a funciones
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Libro {
+    char * titulo;
+    int paginas;
+};
+
 int main(){
     printf("Esta es la actividad 2.\n");
 
